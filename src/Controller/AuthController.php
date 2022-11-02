ce112c8074beabf30631544e93bde00eaa43e346
Mario <mr.babinec@gmail.com>
2022-11-02 15:01:26 +0100

    set default route

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -2,13 +2,32 @@
 
 namespace App\Controller;
 
+use App\Classes\ChangePassword;
+use App\Entity\User;
+use App\Form\ChangePasswordType;
+use App\Form\RegistrationFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class AuthController extends AbstractController
 {
+    /**
+     * @Route("/", name="app_default")
+     */
+    public function index(): Response
+    {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('radio_list');
+        }
+
+        return $this->redirectToRoute('app_login');
+    }
+
     /**
      * @Route("/login", name="app_login")
      */
@@ -29,5 +48,66 @@ class AuthController extends AbstractController
     public function logout(): void
     {
         throw new \LogicException();
+    }
+
+    /**
+     * @Route("/register", name="app_register")
+     */
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+            // do anything else you need here, like send an email
+
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('registration/register.html.twig', [
+            'registrationForm' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/change-password", name="change_password")
+     */
+    public function changePassword(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager)
+    {
+
+        $changePasswordModel = new ChangePassword();
+        $form = $this->createForm(ChangePasswordType::class, $changePasswordModel);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $entityManager->find(User::class, $this->getUser()->getId());
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $user,
+                    $form->get('newPassword')->getData()
+                )
+            );
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('radio_list');
+        }
+
+        return $this->render('registration/changePassword.twig', array(
+            'changePasswordForm' => $form->createView(),
+        ));
     }
 }
